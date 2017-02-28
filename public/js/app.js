a1023c4cb4f9fb9d0a2f9922607823ca500bb4a4
Talon Hughes <talonhughes@dhcp2-171.orem.infotraxsys.com>
2017-02-28 14:49:09 -0700

    added angular material custom color theme for the navbar that is not on home.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,180 +1,171 @@
-var app = angular.module('santaLettersApp', ['ui.router', 'ngMaterial', 'ngAnimate', 'ngSanitize']);
+var app = angular.module('santaLettersApp', ['ui.router', 'ngMaterial', 'ngAnimate', 'ngSanitize', 'ngMessages']);
 
-app.config(function($urlRouterProvider, $stateProvider) {
+app.config(function($urlRouterProvider, $stateProvider, $mdThemingProvider) {
+
+    // ANGULAR MATERIAL CUSTOM THEME COLOR //
+    var myRedColor = $mdThemingProvider.extendPalette('red', {'500': '#D9534F'});
+    // Register the new color palette map with the name <code>myRedColor</code>
+    $mdThemingProvider.definePalette('myRedColor', myRedColor);
+    // Use that theme for the primary intentions
+    $mdThemingProvider.theme('default').primaryPalette('myRedColor');
+
+    // ANGULAR MATERIAL CUSTOM THEME COLOR END//
+
+    // UI ROUTER //
 
     $urlRouterProvider.otherwise('/home');
 
-
-    $stateProvider
-    .state('home', {
-            url: '/home',
-            views: {
-                content: {
-                    templateUrl: 'partials/home.html'
-                },
-
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                }
+    $stateProvider.state('home', {
+        url: '/home',
+        views: {
+            content: {
+                templateUrl: 'partials/home.html'
             },
-            controller: 'carouselCtrl'
-        })
 
-        .state('letterTemplates', {
-            url: '/letterTemplates',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/letterTemplates.html'
-                }
-            },
-            controller: 'letterTemplateCtrl'
-        })
-
-        .state('letterCreate', {
-            url: '/letterWorkshop',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/letterCreate.html'
-                }
-            },
-            controller: 'letterCreateCtrl'
-        })
-
-        .state('letterApproval', {
-            url: '/letterApproval',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/letterApproval.html'
-                }
-            },
-            controller: 'letterWorkshopCtrl'
-        })
-
-        .state('santasShop', {
-            url: '/santasShop',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/santasShop.html'
-                }
-            },
-            controller: 'santasShopCtrl'
-        })
-
-        .state('productDetails', {
-            url: '/productDetails/:id',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/productDetails.html'
-                }
-            },
-            controller: 'productDetailsCtrl'
-        })
-
-        .state('userCart', {
-            url: '/userCart',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/userCart.html'
-                }
-            },
-            controller: 'userCartCtrl'
-        })
-
-        .state('userCheckout', {
-            url: '/userCheckout',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/userCheckout.html'
-                }
-            },
-            controller: 'letterWorkshopCtrl'
-        })
-
-        .state('about', {
-            url: '/about',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/about.html'
-                }
+            footer: {
+                templateUrl: 'templates/footer.html'
             }
-        })
-
-        .state('contact', {
-            url: '/contact',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/contact.html'
-                }
+        },
+        controller: 'carouselCtrl'
+    }).state('letterTemplates', {
+        url: '/letterTemplates',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/letterTemplates.html'
             }
-        })
-
-        .state('howItWorks', {
-            url: '/howItWorks',
-            views: {
-                nav: {
-                    templateUrl: 'templates/nav.html'
-                },
-                footer: {
-                    templateUrl: 'templates/footer.html'
-                },
-                content: {
-                    templateUrl: 'partials/howItWorks.html'
-                }
+        },
+        controller: 'letterTemplateCtrl'
+    }).state('letterCreate', {
+        url: '/letterWorkshop',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/letterCreate.html'
             }
-        });
+        },
+        controller: 'letterCreateCtrl'
+    }).state('letterApproval', {
+        url: '/letterApproval',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/letterApproval.html'
+            }
+        },
+        controller: 'letterWorkshopCtrl'
+    }).state('santasShop', {
+        url: '/santasShop',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/santasShop.html'
+            }
+        },
+        controller: 'santasShopCtrl'
+    }).state('productDetails', {
+        url: '/productDetails/:id',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/productDetails.html'
+            }
+        },
+        controller: 'productDetailsCtrl'
+    }).state('userCart', {
+        url: '/userCart',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/userCart.html'
+            }
+        },
+        controller: 'userCartCtrl'
+    }).state('userCheckout', {
+        url: '/userCheckout',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/userCheckout.html'
+            }
+        },
+        controller: 'letterWorkshopCtrl'
+    }).state('about', {
+        url: '/about',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/about.html'
+            }
+        }
+    }).state('contact', {
+        url: '/contact',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/contact.html'
+            }
+        }
+    }).state('howItWorks', {
+        url: '/howItWorks',
+        views: {
+            nav: {
+                templateUrl: 'templates/nav.html'
+            },
+            footer: {
+                templateUrl: 'templates/footer.html'
+            },
+            content: {
+                templateUrl: 'partials/howItWorks.html'
+            }
+        }
+    });
+
+    // UI ROUTER END//
 
 });
